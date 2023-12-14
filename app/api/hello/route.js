4ace007f520d76f22a1c058b2ff88f552a85a61d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-14 19:26:09 +0900

    Update route.js

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,10 +1,5 @@
-/*
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req) {
   return NextResponse.json({ name: 'John Doe' })
-}
-*/
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
 }
