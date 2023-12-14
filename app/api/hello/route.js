a6c2a9428db1c9ebb5d86f91f9ac45404d41ec86
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-14 19:15:05 +0900

    Update route.js

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,6 +1,12 @@
-
+/*
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
+*/
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
   res.status(200).json({ name: 'John Doe' })
 }
