1f1ab87e74c4fccd8cc52d97bd7ac2e4206e91f3
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-15 14:28:50 +0900

    Update route.js

--- app/api/basic-app-nextjs-app-router/route.js
+++ app/api/basic-app-nextjs-app-router/route.js
@@ -1,1 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req) {
+  const params = req.nextUrl.searchParams;
+  //const location = params.get("location");
+  
+  return NextResponse.json({ name: 'John Doe' })
+}
+
+export async function POST(req) {
+  //const json = await req.json();
+
+  return NextResponse.json({ name: 'John Doe' })
+}
 
