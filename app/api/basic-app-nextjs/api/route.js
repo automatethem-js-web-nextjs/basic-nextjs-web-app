d836910e3a7260e71f14721a738ab7513cd1e72b
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-27 10:00:21 +0900

    Update route.js

--- app/api/basic-app-nextjs/api/route.js
+++ app/api/basic-app-nextjs/api/route.js
@@ -1,31 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-/*
-export async function GET(request) {
-    const params = request.nextUrl.searchParams;
-    const inputs = params.get("inputs");
-    console.log(inputs);
-    //
-    //console.log(request.url); //https://basic-api-nextjs-edge.vercel.app/api/basic-api-nextjs-edge/api?inputs=Hello
-    //const url = new URL(request.url);
-    //const params = url.searchParams;
-    //const inputs = params.get("inputs");
-    //console.log(inputs);
-    
-    return NextResponse.json({ name: 'John Doe' })
-}
-
-export async function POST(request) {
-    const json = await request.json();
-    //console.log(json);
-    //const intent = json["intent"]["name"];
-
-    return NextResponse.json({ name: 'John Doe' })
-}
-
-//export const runtime = 'edge';
-*/
-///*
 export async function GET(request) {
     const params = request.nextUrl.searchParams;
     const inputs = params.get("inputs");
@@ -50,4 +24,3 @@ export async function POST(request) {
 
 //export const runtime = 'nodejs'; //default
 export const runtime = 'edge';
-//*/
