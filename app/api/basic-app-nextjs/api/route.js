6bb97e2609a1d060aef7f8e2a8ff9234a8541ea9
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-27 09:54:27 +0900

    Update route.js

--- app/api/basic-app-nextjs/api/route.js
+++ app/api/basic-app-nextjs/api/route.js
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+/*
 export async function GET(request) {
     const params = request.nextUrl.searchParams;
     const inputs = params.get("inputs");
@@ -19,6 +20,33 @@ export async function POST(request) {
     //console.log(json);
     //const intent = json["intent"]["name"];
 
-  return NextResponse.json({ name: 'John Doe' })
+    return NextResponse.json({ name: 'John Doe' })
 }
 
+//export const runtime = 'edge';
+*/
+///*
+export async function GET(request) {
+    const params = request.nextUrl.searchParams;
+    const inputs = params.get("inputs");
+    console.log(inputs);
+    //
+    //console.log(request.url); //https://basic-api-nextjs-edge.vercel.app/api/basic-api-nextjs-edge/api?inputs=Hello
+    //const url = new URL(request.url);
+    //const params = url.searchParams;
+    //const inputs = params.get("inputs");
+    //console.log(inputs);
+    
+    return NextResponse.json({ name: 'John Doe' })
+}
+
+export async function POST(request) {
+    const json = await request.json();
+    //console.log(json);
+    //const intent = json["intent"]["name"];
+
+    return NextResponse.json({ name: 'John Doe' })
+}
+
+export const runtime = 'nodejs'; //default
+//*/
