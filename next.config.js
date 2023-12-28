adc3ca5a3c5cabcc9fccd455e0098a7b90a38e10
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-12-28 19:46:14 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //reactStrictMode: true, 
+  reactStrictMode: false, //개발 모드일 경우 useEffect가 두번 실행되는 것 방지
+  swcMinify: true,
+}
 
 module.exports = nextConfig
