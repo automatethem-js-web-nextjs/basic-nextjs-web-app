b9b97e2385976cd4ed9eb6560c2c104e7bdf6d94
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-02-13 19:25:48 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+/*
 const nextConfig = {
   //reactStrictMode: true, 
   reactStrictMode: false, //개발 모드일 경우 useEffect가 두번 실행되는 것 방지
@@ -6,3 +7,23 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+*/
+///*
+const path = require("path");
+
+const nextConfig = {
+  //reactStrictMode: true, 
+  reactStrictMode: false, //개발 모드일 경우 useEffect가 두번 실행되는 것 방지
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
+}
+
+module.exports = nextConfig
+//*/
